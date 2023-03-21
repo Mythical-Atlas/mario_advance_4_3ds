cac0ad53edd3259d0d9a177f756f19299cdd74f5
Mythical-Atlas <ben@robertcorrell.com>
2023-03-21 01:11:02 -0400

    More work with audio

--- include/audio.hpp
+++ include/audio.hpp
@@ -7,62 +7,42 @@
 
 #include <SDL2/SDL.h>
 
-#define BUFFER_SIZE 102400
+#define BUFFER_SIZE 1024 // in bytes
 
 using namespace std;
 
-class AudioPlayer {
+// modify to make into a mixer
+// list of filestreams (loaded at runtime?)
+// list of attributes
+// mix all active filestreams in callback
+class AudioFile {
 public:
-    ifstream file;
-    ifstream file2;
+    ifstream fileStream;
     SDL_AudioDeviceID device;
     uint8_t buffer[BUFFER_SIZE];
-    uint8_t buffer2[BUFFER_SIZE];
 
     static void queueAudio(void* userdata, uint8_t* stream, int length) {
-        AudioPlayer* player = (AudioPlayer*)userdata;
+        AudioFile* audioFile = (AudioFile*)userdata;
 
         memset(stream, 0, length);
-        memset(player->buffer, 0, BUFFER_SIZE);
-        memset(player->buffer2, 0, BUFFER_SIZE);
+        memset(audioFile->buffer, 0, BUFFER_SIZE);
 
-        cout << player->file2.tellg() << endl;
+        audioFile->fileStream.read((char*)audioFile->buffer, length);
+        SDL_MixAudioFormat(stream, audioFile->buffer, AUDIO_S16, audioFile->fileStream.gcount(), SDL_MIX_MAXVOLUME);
 
-        player->file.read((char*)player->buffer, length);
-        player->file2.read((char*)player->buffer2, player->file.gcount());
-
-        //if(player->file.gcount() < length) {memset(&(player->buffer[player->file.gcount()]), 0, length - player->file.gcount());}
-        //if(player->file2.gcount() < length) {memset(&(player->buffer2[player->file2.gcount()]), 0, length - player->file2.gcount());}
-
-        SDL_MixAudioFormat(stream, player->buffer, AUDIO_S16, player->file.gcount(), SDL_MIX_MAXVOLUME);
-        SDL_MixAudioFormat(stream, player->buffer2, AUDIO_S16, player->file.gcount(), SDL_MIX_MAXVOLUME);
-
-        /*if(player->file.gcount() >= player->file2.gcount()) {
-            SDL_MixAudioFormat(player->buffer, player->buffer2, AUDIO_S16, player->file2.gcount(), SDL_MIX_MAXVOLUME);
-            SDL_MixAudioFormat(stream, player->buffer, AUDIO_S16, player->file.gcount(), SDL_MIX_MAXVOLUME);
-        }
-        else {
-            SDL_MixAudioFormat(player->buffer2, player->buffer, AUDIO_S16, player->file.gcount(), SDL_MIX_MAXVOLUME);
-            SDL_MixAudioFormat(stream, player->buffer2, AUDIO_S16, player->file2.gcount(), SDL_MIX_MAXVOLUME);
-        }*/
-
-        cout << player->file2.gcount() << endl;
-
-        if(player->file2.tellg() == -1) {
-            cout << "eof reached" << endl;
-            SDL_PauseAudioDevice(player->device, 1);
-        }
-
-        if(player->file.tellg() == -1) {
-            cout << "sounds effect eof reached" << endl;
-
+        /*if(player->file.tellg() == -1 && player->file2.tellg() == -1) {
             player->file.clear();
             player->file.seekg(0, ios::beg);
 
-            SDL_PauseAudioDevice(player->device, 1);
-        }
+            player->file2.clear();
+            player->file2.seekg(0, ios::beg);
 
-        cout << "\n" << endl;
+            SDL_PauseAudioDevice(player->device, 1);
+        }*/
+        if(audioFile->fileStream.tellg() == -1) {
+            audioFile->fileStream.clear();
+            audioFile->fileStream.seekg(0, ios::beg);
+        }
     }
 
     void init() {
@@ -80,7 +60,7 @@ public:
         device = SDL_OpenAudioDevice(NULL, 0, &spec, NULL, 0);
 
         file = ifstream("romfs/grow.raw", ios::binary);
-        file2 = ifstream("romfs/music11.raw", ios::binary);
+        file2 = ifstream("romfs/shortMusic.raw", ios::binary);
 
         SDL_PauseAudioDevice(device, 0);
     }
