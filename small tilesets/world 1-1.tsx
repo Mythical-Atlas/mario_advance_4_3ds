643bced1224590796440af6732ba6e7d5c358176
Mythical-Atlas <ben@robertcorrell.com>
2023-04-05 08:21:16 -0400

    Sprite and map work

---
+++ small tilesets/world 1-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="world 1-1" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="world 1-1.png" width="256" height="256"/>
+</tileset>
